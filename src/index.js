e9fa1f577386a4982b8bcf21d612f776bfae1380
Chris Heninger <chrish@synapsestudios.com>
2017-03-18 23:20:20 -0700

    0.0.4 - Export all utils and get a working version published

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import DropNCrop from './DropNCrop';
 
+import bytesToSize from './util/bytesToSize';
+import dataUrlToFile from './util/dataUrlToFile';
+import fileSizeLessThan from './util/fileSizeLessThan';
+import fileType from './util/fileType';
+
+export { bytesToSize, dataUrlToFile, fileSizeLessThan, fileType };
+
 export default DropNCrop;
